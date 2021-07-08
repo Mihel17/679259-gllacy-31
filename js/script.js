8c9a9af49a6b21fb954b0dfed489cf25bfacafd2
Мигель <83421739+Mihel17@users.noreply.github.com>
2021-07-08 22:39:15 +0300

    Правки

---
+++ js/script.js
@@ -0,0 +1,48 @@
+const body = document.querySelector(".page-body");
+const sliderBtn1 = document.querySelector(".promo__controllers-btn-1");
+const sliderBtn2 = document.querySelector(".promo__controllers-btn-2");
+const sliderBtn3 = document.querySelector(".promo__controllers-btn-3");
+const slide1 = document.querySelector(".promo__slide-1");
+const slide2 = document.querySelector(".promo__slide-2");
+const slide3 = document.querySelector(".promo__slide-3");
+
+sliderBtn1.addEventListener("click", function(evt){
+  evt.preventDefault();
+  body.classList.remove("page-body--color2");
+  body.classList.remove("page-body--color3");
+  slide1.classList.add("promo__slide--current");
+  slide2.classList.remove("promo__slide--current");
+  slide3.classList.remove("promo__slide--current");
+  sliderBtn1.classList.add("promo__controllers-btn--current");
+  sliderBtn2.classList.remove("promo__controllers-btn--current");
+  sliderBtn3.classList.remove("promo__controllers-btn--current");
+  console.log("click1");
+});
+
+
+sliderBtn2.addEventListener("click", function(evt){
+  evt.preventDefault();
+  body.classList.add("page-body--color2");
+  body.classList.remove("page-body--color3");
+  slide1.classList.remove("promo__slide--current");
+  slide2.classList.add("promo__slide--current");
+  slide3.classList.remove("promo__slide--current");
+  sliderBtn1.classList.remove("promo__controllers-btn--current");
+  sliderBtn2.classList.add("promo__controllers-btn--current");
+  sliderBtn3.classList.remove("promo__controllers-btn--current");
+  console.log("click2");
+});
+
+sliderBtn3.addEventListener("click", function(evt){
+  evt.preventDefault();
+  body.classList.remove("page-body--color2");
+  body.classList.add("page-body--color3");
+  slide1.classList.remove("promo__slide--current");
+  slide2.classList.remove("promo__slide--current");
+  slide3.classList.add("promo__slide--current");
+  sliderBtn1.classList.remove("promo__controllers-btn--current");
+  sliderBtn2.classList.remove("promo__controllers-btn--current");
+  sliderBtn3.classList.add("promo__controllers-btn--current");
+  console.log("click3");
+});
+
